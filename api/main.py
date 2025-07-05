8f4720ed7f06ccc082b032f76f75dcb533049881
Masakazu Takai <masakazu.t0531@gmail.com>
2025-07-05 10:44:57 +0900

    元に戻します。

--- api/main.py
+++ api/main.py
@@ -6,4 +6,4 @@ BLOB_READ_WRITE_TOKEN = os.getenv("BLOB_READ_WRITE_TOKEN")
 
 @app.get("/")
 async def root():
-    return {"message": f"わたしはカモメです。名前はまだありません。{BLOB_READ_WRITE_TOKEN}"}
+    return {"message": f"わたしはカモメです。名前はまだありません。"}
